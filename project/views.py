a163bffb7c63df866b48aaef3fc7be8842d7bfdd
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2021-12-23 13:23:30 -0600

    Move object AppController

--- project/views.py
+++ project/views.py
@@ -2,14 +2,16 @@ from flask import render_template
 from project import app
 from project.app_controller import AppController
 
-CONTROLLER = AppController()
 
 @app.get("/")
 def index():
+    CONTROLLER = AppController()
     data = CONTROLLER.get_pages()
     return render_template("index.html", value=data)
 
+
 @app.get("/filter_fields")
 def get_filter_fields():
+    CONTROLLER = AppController()
     """Show possible filter fields. ONLY DEVELOPMENT METHOD, DELETE IN PRODUCTION"""
     return render_template("index.html", value=CONTROLLER.get_filters_fields())
